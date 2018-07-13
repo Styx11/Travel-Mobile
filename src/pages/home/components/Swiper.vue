9c4bb537f3bef4e97a9382ffcdbdd1896846c1e5
Styx11 <1654630248@qq.com>
2018-07-13 23:08:53 +0800

    icons

--- src/pages/home/components/Swiper.vue
+++ src/pages/home/components/Swiper.vue
@@ -51,7 +51,7 @@ export default {
     overflow: hidden
     width: 100%
     height: 0
-    padding-bottom: 31.25%
+    padding-bottom: 26.80%
     background: #eee
     .swiper-img
       width: 100%
